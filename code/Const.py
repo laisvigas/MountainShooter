374c5220f383cf30c5948946c4c5b050a80d2958
lais <laisvigas@outlook.com>
2024-08-19 12:08:27 -0300

    adding features in Level.py file

--- code/Const.py
+++ code/Const.py
@@ -8,15 +8,31 @@ COLOR_GREEN = (0, 128, 0)
 COLOR_CYAN = (0, 128, 128)
 
 # E
+EVENT_ENEMY = pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 ENTITY_SPEED = {
-    "Level1Bg0": 0,
-    "Level1Bg1": 1,
-    "Level1Bg2": 2,
-    "Level1Bg3": 3,
-    "Level1Bg4": 4,
-    "Level1Bg5": 5,
-    "Level1Bg6": 6,
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 3,
+    'Level1Bg4': 4,
+    'Level1Bg5': 5,
+    'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
+    'Player1': 3,
+    'Player1Shot': 1,
+    'Player2': 3,
+    'Player2Shot': 3,
+    'Enemy1': 1,
+    'Enemy1Shot': 5,
+    'Enemy2': 1,
+    'Enemy2Shot': 2,
 }
+
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
@@ -24,6 +40,13 @@ MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
+
+# S
+SPAWN_TIME = 4000
+
+# T
+TIMEOUT_STEP = 100  # 100ms
+TIMEOUT_LEVEL = 20000  # 20s
 
 # W
 WIN_WIDTH = 576
