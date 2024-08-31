85efa3918cf31c9c79ce4c60728ed06d9069e309
lais <laisvigas@outlook.com>
2024-08-31 09:12:07 -0300

    creating players and enemies. Moving players and enemies.

--- code/Const.py
+++ code/Const.py
@@ -41,6 +41,18 @@ MENU_OPTION = ('NEW GAME 1P',
                'SCORE',
                'EXIT')
 
+# P
+PLAYER_KEY_UP = {'Player1': pygame.K_UP,
+                 'Player2': pygame.K_w}
+PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
+                   'Player2': pygame.K_s}
+PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
+                   'Player2': pygame.K_a}
+PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
+                    'Player2': pygame.K_d}
+PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
+                    'Player2': pygame.K_LCTRL}
+
 # S
 SPAWN_TIME = 4000
 
